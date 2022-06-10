3c8a0c06d66a2803ba91118fb1116318af662986
Akin Karayun <akinkarayun1@hotmail.com>
2022-06-10 16:24:52 +0300

    created simple animated moving box simple

--- App.tsx
+++ App.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, StyleSheet, View} from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
+
+function Box() {
+  const offset = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{translateX: offset.value * 255}],
+    };
+  });
+
+  return (
+    <>
+      <Animated.View style={[styles.box, animatedStyles]} />
+      <Button onPress={() => (offset.value = Math.random())} title="Move" />
+    </>
+  );
+}
 
 const App = () => {
-  return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  );
+  return <Box />;
 };
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    aspectRatio: 1,
+    backgroundColor: 'red',
+    marginVertical: 50,
+  },
+});
 
 export default App;
